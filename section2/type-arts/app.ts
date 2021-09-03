517192d45ff3b405d8a0c25ac234abbde11326e0
Szymon Szaminko <sz.szaminko@gmail.com>
2021-09-03 15:51:05 +0200

    Create example to types Number, String and Boolean

---
+++ section2/type-arts/app.ts
@@ -0,0 +1,37 @@
+//----- Number -----
+//type number
+let price: number;
+price = 9.95;
+//type bigint
+let big: bigint;
+big = 9007199254740991n;
+
+//----- String -----
+
+let firstName: string = `John`;
+let title: string = `Web Developer`;
+let profile: string = `I'm ${firstName}. 
+I'm a ${title}`;
+
+//----- Boolean -----
+
+let isGood: boolean;
+isGood = true;
+
+//----- Object -----
+
+//----- Array -----
+
+//----- Tuple -----
+
+//----- Enum -----
+
+//----- Any -----
+
+//----- Never -----
+
+//----- Union -----
+
+//----- Type Aliases -----
+
+//----- String Literal Types -----
