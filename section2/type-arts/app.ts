74ab3202cd62eab7fa8dfd0e2a2458e14a387b76
Szymon Szaminko <sz.szaminko@gmail.com>
2021-09-04 11:16:56 +0200

    Create example about Enum type

--- section2/type-arts/app.ts
+++ section2/type-arts/app.ts
@@ -67,6 +67,57 @@ let color: [number, number, number] = [255, 0, 0];
 
 //----- Enum -----
 
+//Example 1
+enum Month {
+  Jan,
+  Feb,
+  Mar,
+  Apr,
+  May,
+  Jun,
+  Jul,
+  Aug,
+  Sep,
+  Oct,
+  Nov,
+  Dec
+};
+
+function isItSummer(month: Month) {
+  let isSummer: boolean;
+  switch (month) {
+    case Month.Jun:
+    case Month.Jul:
+    case Month.Aug:
+      isSummer = true;
+      break;
+    default:
+      isSummer = false;
+      break;
+  }
+  return isSummer;
+}
+//console.log(isItSummer(Month.Jun)); // true
+
+//2
+enum ApprovalStatus {
+  draft,
+  submitted,
+  approved,
+  rejected
+};
+
+const request = {
+  id: 1,
+  status: ApprovalStatus.approved,
+  description: 'Please approve this request'
+};
+
+if (request.status === ApprovalStatus.approved) {
+  // send an email
+  console.log('Send email to the Applicant...');
+}
+
 //----- Any -----
 
 //----- Never -----
