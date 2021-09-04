097f9d3893a4bac8fa9be86323acd6dd5765eb45
Szymon Szaminko <sz.szaminko@gmail.com>
2021-09-04 09:51:34 +0200

    Create example about object type

--- section2/type-arts/app.ts
+++ section2/type-arts/app.ts
@@ -20,6 +20,31 @@ isGood = true;
 
 //----- Object -----
 
+//1 - allowed but not good practice
+let employee1: object;
+
+employee1 = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 25,
+  jobTitle: 'Web Developer'
+};
+
+//2 - good practice
+let employee2: {
+  firstName: string;
+  lastName: string;
+  age: number;
+  jobTitle: string;
+};
+
+employee2 = {
+  firstName: 'James',
+  lastName: 'Dean',
+  age: 33,
+  jobTitle: 'Backend Developer'
+};
+
 //----- Array -----
 
 //----- Tuple -----
