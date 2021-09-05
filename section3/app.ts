3852150b1c85617f655ec0a2e7a736f80f1a1e6a
Szymon Szaminko <sz.szaminko@gmail.com>
2021-09-05 05:55:39 +0200

    Create order and file for examplew with condition flow statements in TypeScript

---
+++ section3/app.ts
@@ -0,0 +1,15 @@
+/*CONTROL FLOW STATEMENTS*/
+
+// ----- if else -----
+
+// ----- switch case -----
+
+// ----- for loop -----
+
+// ----- while loop -----
+
+// ----- do while loop -----
+
+// ----- break -----
+
+
