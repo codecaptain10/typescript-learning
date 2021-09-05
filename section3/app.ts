4f529207961b4a9d6d0ca92f90c8fd8a8fb95121
Szymon Szaminko <sz.szaminko@gmail.com>
2021-09-05 06:20:05 +0200

    Create example about if else condition

--- section3/app.ts
+++ section3/app.ts
@@ -2,6 +2,27 @@
 
 // ----- if else -----
 
+let discount: number;
+let itemCount = 11;
+
+if (itemCount > 0 && itemCount <= 5) {
+  discount = 5;  // 5% discount
+} else if (itemCount > 5 && itemCount <= 10) {
+  discount = 10; // 10% discount 
+} else {
+  discount = 15; // 15%
+}
+
+console.log(`You got ${discount}% discount. `)
+
+//if else - Ternary operator
+const max = 100;
+let counter = 100;
+
+counter < max ? counter++ : counter = 1;
+
+console.log(counter);
+
 // ----- switch case -----
 
 // ----- for loop -----
