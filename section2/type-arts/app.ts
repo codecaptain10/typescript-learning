20662383d7586a561db2da1464261267586f7e9d
Szymon Szaminko <sz.szaminko@gmail.com>
2021-09-05 04:35:58 +0200

    Update example about union type

--- section2/type-arts/app.ts
+++ section2/type-arts/app.ts
@@ -150,6 +150,25 @@ function raiseError(message: string): never {
 }
 //----- Union -----
 
+//Example 1
+let scoreResult: number | string;
+scoreResult = 10; // OK
+scoreResult = 'Hi'; // also OK
+//scoreResult = false; // a boolean value, not OK
+
+//Example 2
+function add(a: number | string, b: number | string) {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b;
+  }
+  if (typeof a === 'string' && typeof b === 'string') {
+    return a.concat(b);
+  }
+  throw new Error('Parameters must be numbers or strings');
+}
+
+
+
 //----- Type Aliases -----
 
 //----- String Literal Types -----
