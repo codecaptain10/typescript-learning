f2c3bd8469eca9fa826753b601dd8eb756865d7c
Szymon Szaminko <sz.szaminko@gmail.com>
2021-09-06 15:32:37 +0200

    Create examples about function overloading in TypeScript

--- section4/app.ts
+++ section4/app.ts
@@ -70,5 +70,57 @@ console.log(applyDiscount(100)); // 95
 
 
 // ------- Rest Parameters -------
+/*Syntax:
+function fn(...rest: type[]) {
+   //...
+}
+*/
+function getTotal(...numbers: number[]): number {
+  let total = 0;
+  numbers.forEach((num) => total += num);
+  return total;
+}
+
+//console.log(getTotal()); // 0
+//console.log(getTotal(10, 20)); // 30
+//console.log(getTotal(10, 20, 30)); // 60
+
 
 // ------- Funtions Overloadings -------
+//Overloading
+function addVariables(a: number, b: number): number;
+function addVariables(a: string, b: string): string;
+function addVariables(a: any, b: any): any {
+  return a + b;
+}
+
+//Overloading with optional parameters
+function sum(a: number, b: number): number;
+function sum(a: number, b: number, c: number): number;
+function sum(a: number, b: number, c?: number): number {
+  if (c) return a + b + c;
+  return a + b;
+}
+
+//Overloading count() in class Connter
+class Counter {
+  private current: number = 0;
+  count(): number;
+  count(target: number): number[];
+  count(target?: number): number | number[] {
+    if (target) {
+      let values = [];
+      for (let start = this.current; start <= target; start++) {
+        values.push(start);
+      }
+      this.current = target;
+      return values;
+    }
+    return ++this.current;
+  }
+}
+
+let counter = new Counter();
+
+console.log(counter.count()); // return a number
+console.log(counter.count(20)); // return an array
