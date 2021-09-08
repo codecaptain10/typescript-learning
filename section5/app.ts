277065f2d65a9d12719caf7e6cea97e04b63d1e0
Szymon Szaminko <sz.szaminko@gmail.com>
2021-09-08 19:32:27 +0200

    Create example with abstract class in TypeScript

--- section5/app.ts
+++ section5/app.ts
@@ -165,3 +165,15 @@ class Worker {
 }
 
 // ------- Abstract Classes -------
+
+abstract class newEmployee {
+  constructor(private firstName: string, private lastName: string) {
+  }
+  abstract getSalary(): number
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+  compensationStatement(): string {
+    return `${this.fullName} makes ${this.getSalary()} a month.`;
+  }
+}
