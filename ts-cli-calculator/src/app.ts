a9626cf384317bc550e9559be52166a6f0b052dc
Szymon Szaminko <sz.szaminko@gmail.com>
2022-02-02 09:57:37 +0100

    Create the project structure and first run in console

--- ts-cli-calculator/src/app.ts
+++ ts-cli-calculator/src/app.ts
@@ -1,2 +1,9 @@
-let message: string = "Hello World!";
-console.log(message);
+import { question } from "readline-sync";
+
+function main(): void {
+  const firstStr: string = question("Enter first number:\n");
+
+  console.log(firstStr);
+}
+
+main();
