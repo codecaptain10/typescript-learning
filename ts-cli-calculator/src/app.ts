8900501b5a81906a785db05f5c42e5badb4b2c35
Szymon Szaminko <sz.szaminko@gmail.com>
2022-02-02 10:04:53 +0100

    Run app without result logic

--- ts-cli-calculator/src/app.ts
+++ ts-cli-calculator/src/app.ts
@@ -1,9 +1,18 @@
+/*CLI Calculator
+1) Start the project
+2) In console run: >node ./dist/app.js
+3) Give parameters
+4) Become result
+
+*/
 import { question } from "readline-sync";
 
 function main(): void {
   const firstStr: string = question("Enter first number:\n");
+  const operator: string = question("Enter operator: \n");
+  const secondStr: string = question("Enter second number:\n");
 
-  console.log(firstStr);
+  console.log("Result:\n" + firstStr, operator, secondStr);
 }
 
 main();
